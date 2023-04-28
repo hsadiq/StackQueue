047325bdb92bab8570da6e1655849fe10e6bd3b2
hsadiq <ksadik320@gmail.com>
2023-04-28 18:23:47 +0530

    Added Creating Queue in LinkedList

---
+++ src/com/bl/Stack/Queue.java
@@ -0,0 +1,15 @@
+package com.bl.Stack;
+
+import java.util.LinkedList;
+
+public class Queue {
+    public static void main(String[] args) {
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        queue.push(56);
+        queue.push(30);
+        queue.push(70);
+
+        System.out.print(queue);
+    }
+}
